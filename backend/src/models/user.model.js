7334a7259430fb238f2292c0bb18e1eb2aaeae06
BuddhadebKoner <jeetkoner36@gmail.com>
2024-08-15 09:19:28 +0530

    added is active section

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -69,6 +69,10 @@ const userSchema = new Schema(
       refreshToken: {
          type: String,
       },
+      isActive: {
+         type: Boolean,
+         default: false,
+      },
    },
    { timestamps: true }
 );
