1cd652bfb54a96b99858e9d2bd8388892f244f60
BuddhadebKoner <jeetkoner36@gmail.com>
2024-09-06 23:11:02 +0530

    bugg fixes

--- frontend/src/pages/Publicpost.jsx
+++ frontend/src/pages/Publicpost.jsx
@@ -15,13 +15,17 @@ export default function Pages() {
       <>
          {publicPost ? (
             <div className='publicpost_container'>
-               <div className="heading_container_public_post_page">
-                  <h1>For any payment or  contact to developer related issue contact us</h1>
-               </div>
                <div className="publicpost_feature_product_container">
-                  <h1>Try the Product</h1>
+                  <div className="publicpost_container_heading_container">
+                     <h1>Try the Product</h1>
+                  </div>
                   <div className="publicpost_ifream_container">
                      <iframe src={publicPost.projectLink} frameborder="0"></iframe>
+                  </div>
+               </div>
+               <div className="publicpost_feature_product_details_container">
+                  <div className="title_order_btn">
+                     <h1>{publicPost.title}</h1>
                   </div>
                </div>
             </div>
